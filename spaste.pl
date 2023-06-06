fbffa3e81ad088ceca670755d9610634d9de67f5
Marshall Whittaker <marshall@oxagast.org>
2023-06-06 10:28:46 -0400

    Added a pidfile check and some signal handlers for proper exits on failure.

--- spaste.pl
+++ spaste.pl
@@ -12,7 +12,6 @@
 # webserver.
 # useage: ./spaste.pl
 
-
 use strict;
 use warnings;
 use IO::Handle;
@@ -22,16 +21,27 @@ use IO::Socket::SSL;
 use threads;
 STDOUT->autoflush();
 STDERR->autoflush();
-my $logfile = "/var/log/spaste.log";                  # log
-my $proot   = "/var/www/spaste.oxasploits.com/p/";    # paste root if not default
-my $host    = "spaste.oxasploits.com";                # change to your server
-my $srvname = "https://" . $host;
-my $port    = "8888";
-my $cer = "/etc/letsencrypt/live/" . $host . "/fullchain.pem";       # use your cert
-my $key = "/etc/letsencrypt/live/" . $host . "/privkey.pem";    # use your privkey
+my ($logfile, $proot, $host, $srvname, $port, $cer, $key, $pidfile);
+$logfile = "/var/log/spaste.log";                  # log
+$proot   = "/var/www/spaste.oxasploits.com/p/";    # paste root if not default
+$host    = "spaste.oxasploits.com";                # change to your server
+$srvname = "https://" . $host;
+$port    = "8888";
+$cer     = "/etc/letsencrypt/live/" . $host . "/fullchain.pem";    # use your cert
+$key     = "/etc/letsencrypt/live/" . $host . "/privkey.pem";      # use your privkey
+$pidfile = "/var/run/spaste.pid";
+$SIG{TERM} = $SIG{INT} = sub { unlink($pidfile); die "Caught a sigterm $!" };
 my $ver = "v0.5";
+
+if (-e $pidfile) {
+  die
+"SPaste is already running or the lockfile didn't get wiped!  If you are sure it is not running, remove $pidfile";
+}
+open(PIDF, ">", $pidfile) or die $!;
+print PIDF $$ . "\n";
+close(PIDF);
 open(STDERR, ">>", $logfile) or die $!;
-open(LOG, '>>', $logfile) or die $!;
+open(LOG,    '>>', $logfile) or die $!;
 LOG->autoflush();
 my $datet = purdydate();
 print LOG "$datet Starting spaste $ver using $host:$port\n";
@@ -43,11 +53,11 @@ my $sock = IO::Socket::IP->new(
                                ReuseAddr => 1
 ) or die "$datet $!";
 umask(022);
-my $WITH_THREADS = 1;                                           # the switch!!
+my $WITH_THREADS = 1;    # the switch!!
 
 while (1) {
   eval {
-    my $cl = $sock->accept();                                   # threaded accept
+    my $cl = $sock->accept();    # threaded accept
     if ($cl) {
       $datet = purdydate();
       my $th = threads->create(\&client, $cl) or die "$datet $!";
@@ -59,24 +69,25 @@ while (1) {
     exit(1);
   }
 }    # forever
-  close(LOG);
-  close(STDERR);
-
+close(LOG);
+close(STDERR);
 
 
 sub client    # worker
 {
   my $cl = shift;
+
   # upgrade INET socket to SSL
   $cl = IO::Socket::SSL->start_SSL(
                                    $cl,
-                                   SSL_server    => 1,
-                                   SSL_cert_file => $cer,
-                                   SSL_key_file  => $key,
-				   SSL_verifycn_name => $host,
-    				   SSL_verifycn_scheme => 'default',
-                                   SSL_hostname => $host
+                                   SSL_server          => 1,
+                                   SSL_cert_file       => $cer,
+                                   SSL_key_file        => $key,
+                                   SSL_verifycn_name   => $host,
+                                   SSL_verifycn_scheme => 'default',
+                                   SSL_hostname        => $host
   ) or die "$datet $@";
+
   # unblock
   my $flags = fcntl($cl, F_GETFL, 0) or die "$datet $cl->peerhost $!";
   fcntl($cl, F_SETFL, $flags | O_NONBLOCK) or die "$datet $cl->peerhost $!";
@@ -87,10 +98,10 @@ sub client    # worker
       my $rndid = "";
       while (1) {
         $rndid = genuniq();
-        if (! -e "$proot$rndid") {
+        if (!-e "$proot$rndid") {
           writef($rndid, $recv, $cl, $logfile);
-          $cl->close() or die "$datet $cl->peerhost $!";                   # close last sock and move on
-          return 0;                       # return so we don't get stuck in the loop
+          $cl->close() or die "$datet $cl->peerhost $!";   # close last sock and move on
+          return 0;    # return so we don't get stuck in the loop
         }
       }
     }
@@ -109,7 +120,7 @@ sub writef() {
   print LOG " $rndid : serving at $srvname/p/$rndid\n";
   print "$rndid : serving at $srvname/p/$rndid\n";
   my $filename = $proot . $rndid;
-  open(P, '>', $filename) or die "$datet $cl->peerhost $!";;
+  open(P, '>', $filename) or die "$datet $cl->peerhost $!";
   print P $recv;
   close(P);
   print $cl "$srvname/p/$rndid" . "\n" or die "$datet $cl->peerhost $!";
@@ -125,10 +136,18 @@ sub genuniq {
   return $pasid;    # push it back
 }
 
+
 sub purdydate {
 
-    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
-    my $datetime = sprintf ( "%04d%02d%02d %02d:%02d:%02d",
-                                   $year+1900,$mon+1,$mday,$hour,$min,$sec);
-    return $datetime;
+  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
+  my $datetime = sprintf("%04d%02d%02d %02d:%02d:%02d",
+                         $year + 1900,
+                         $mon + 1, $mday, $hour, $min, $sec);
+  return $datetime;
+}
+
+
+END {
+  print "Something unusual happened... check $logfile";
+  unlink($pidfile);
 }
