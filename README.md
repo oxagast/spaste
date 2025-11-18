702cb390be5d7a177d05aea22b8a09d4b86b0a80
Marshall Whittaker <oxagast@linuxmail.org>
2025-11-17 20:04:52 -0500

    Added installer and install instructions.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-A pure perl SSL encrypted port of [termbin.com](https://github.com/solusipse/fiche).
+#Spaste
+##A pure perl SSL encrypted port of [termbin.com](https://github.com/solusipse/fiche).
 
+###Notes
 For the server I suggest adding an ssl group and spaste user, the systemd startup/respawn script if its a dedicated setup, and the crontab entry that removes expired pastes, for security purposes.
 
-The client is more simple:
+###Deps
+openssl or perl
 
-Install (perl client): `sudo cp paste-client.pl /usr/local/bin/sp`
+###Install
+To install, run `install.sh`
 
-Install (openssl client with zsh): `echo "spaste() {timeout 1s openssl s_client -quiet -servername spaste.oxasploits.com -verify_return_error -connect spaste.oxasploits.com:8866 2>/dev/null | grep -v "END" | tr -d '\\\\n'; echo}" >> ~/.zshrc && source ~/.zshrc`
-
+###Use
 Useage: `echo "yes hello" | sp`
 
+### Thanks
 Thanks for a bugfix, brianx!
--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "Installing SPaste client..."
+echo "Preparing to install SPaste client..."
+if ! command -v "openssl" 2>&1 >/dev/null; then
+  echo "Please first install the openssl package for your distro, this will not work without it!"
+  echo "   Redhat based:    $ sudo dnf install openssl"
+  echo "   Debian based:    $ sudo apt install openssl"
+  echo "   Arch based:      $ sudo pacman -S openssl"
+  exit 1
+fi
 SPSTR=YWxpYXMgc3A9InRpbWVvdXQgMXMgb3BlbnNzbCBzX2NsaWVudCAtcXVpZXQgLXNlcnZlcm5hbWUgc3Bhc3RlLm94YXNwbG9pdHMuY29tIC12ZXJpZnlfcmV0dXJuX2Vycm9yIC1jb25uZWN0IHNwYXN0ZS5veGFzcGxvaXRzLmNvbTo4ODY2IDI+L2Rldi9udWxsIHwgZ3JlcCAtdiBFTkQgfCB0ciAtZCAnXG4nOyBlY2hvIgo=
 SHF=""
 
@@ -10,21 +17,27 @@ isinst() {
   fi
 }
 
+installit() {
+  echo $SPSTR | base64 -d >> $SHF
+  echo "Sucessfully installed!"
+}
+
+
 if [[ $(ps -p $PPID | grep -v CMD | awk '{print $NF}') == "bash" ]]; then
   SHF="$HOME/.bashrc"
   isinst
-  echo $SPSTR | base64 -d >> $SHF
-  echo "Installed for bash!"
+  if [[ -v BASH_ALIASES[sp] ]]; then
+   echo defined
+  fi
+  installit
 elif [[ $(ps -p $PPID | grep -v CMD | awk '{print $NF}') == "zsh" ]]; then
   SHF="$HOME/.zshrc"
   isinst
-  echo $SPSTR | base64 -d >> $SHF
-  echo "Installed for zsh!"
+  installit
 elif [[ $(ps -p $PPID | grep -v CMD | awk '{print $NF}') == "sh" ]]; then
   SHF="$HOME/.profile"
   isinst
-  echo $SPSTR | base64 -d >> $SHF
-  echo "Installed for sh!"
+  instalit
 else
   echo "Couldn't detect shell, either manually add an alias, or use the spaste-client.pl script!"
   exit 1
