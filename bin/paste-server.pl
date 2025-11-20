d5321f39effaad97c2bed6176728d78971d28276
Marshall Whittaker <marshall@oxasploits.com>
2025-11-20 13:58:11 -0500

    Changes to logging.

--- bin/paste-server.pl
+++ bin/paste-server.pl
@@ -24,15 +24,16 @@ use IO::Socket::SSL;
 use threads;
 use Config::Tiny;
 use Getopt::Long qw (GetOptions);
-$SIG{TERM} = $SIG{INT} = sub { die "Caught a sigterm $!" };
+$SIG{TERM} = $SIG{INT} = sub {die "Caught a sigterm $!"};
 STDOUT->autoflush();
 STDERR->autoflush();
 
 if ($#ARGV + 1 ne 2) {
-  print "Incorrect number of arguments.\n Useage:\n  $ARGV[0] --conf [file]\n";
+  print STDOUT "Incorrect number of arguments.\n Useage:\n  $ARGV[0] --conf [file]\n";
   exit $SIG{TERM};
 }
-my ($logfile, $pasteroot, $host, $srvname, $port, $certfile, $keyfile, $pidfile);
+my ($logfile, $pasteroot, $host, $srvname, $port, $certfile, $keyfile,
+  $pidfile);
 my $cfgf = undef;
 GetOptions('conf=s' => \$cfgf);
 my $config = Config::Tiny->read($cfgf);
@@ -44,17 +45,17 @@ $keyfile   = $config->{SSL}{keyfile};
 $pidfile   = $config->{Settings}{pidfile};
 $pasteroot = $config->{Server}{pasteroot};
 $logfile   = $config->{Settings}{logfile};    # log
-my $ver = "v1.1";                             # hell yea, new revision!
+my $ver = "v1.2.1";                           # hell yea, new revision!
                                               # can we have a party
                                               # with lots of hookers?
                                               # bonus points for anal beads
-
 if (-e $pidfile) {
   die
 "0x07 Error: SPaste is already running or the lockfile didn't get wiped!  If you are sure it is not running, remove $pidfile";
 }
 if ($srvname =~ m|http:|) {
-  print STDERR "0x09 Error: The baseuri should not be http! Only use a properly configured https server with a fqdn here!\n";
+  print STDERR
+"0x09 Error: The baseuri should not be http! Only use a properly configured https server with a fqdn here!\n";
 }
 open(PIDF, ">", $pidfile) or die $!;
 print PIDF $$ . "\n";
@@ -63,16 +64,16 @@ open(STDERR, ">>", $logfile) or die $!;
 open(LOG,    '>>', $logfile) or die $!;
 LOG->autoflush();
 my $datet = purdydate();
-print LOG "$datet Starting spaste $ver using $host:$port\n";
+print LOG "$datet 0x00 Starting spaste $ver using $host:$port\n";
 my $siteroot = $pasteroot;
 $siteroot =~ s|/p/$||;
-chdir "$siteroot" or die "$datet $!";
+chdir "$siteroot" or die "$datet 0x0A $!";
 my $sock = IO::Socket::IP->new(
-                               Listen    => SOMAXCONN,
-                               LocalPort => $port,
-                               Blocking  => 1,
-                               ReuseAddr => 1
-) or die "0x08 Error: $datet $!";
+  Listen    => SOMAXCONN,
+  LocalPort => $port,
+  Blocking  => 1,
+  ReuseAddr => 1)
+  or print LOG "0x08 Error: $datet $!";
 umask(022);
 my $WITH_THREADS = 1;    # the switch!!
 while (1) {
@@ -80,76 +81,69 @@ while (1) {
     my $cl = $sock->accept();    # threaded accept
     if ($cl) {
       $datet = purdydate();
-      my $th = threads->create(\&server, $cl) or die "$datet 0x06 Error: $!";
-      $th->detach() or print LOG "$datet 0x05 Error: Thread detach request failed. $!\n";
+      my $th = threads->create(\&server, $cl) or print LOG "$datet 0x06 Error: $!";
+      $th->detach()
+        or print LOG "$datet 0x05 Error: Thread detach request failed. $!\n";
     }
   };    # eval
   if ($@) {
-    print STDERR "0x04 Error: $@\n";
+    print STDERR purdydate() . " 0x04 Error: $!\n";
     exit(1);
   }
 }    # forever
 close(LOG);
 close(STDERR);
 
-
 sub server {
   my $cl = shift;
   # upgrade INET socket to SSL
   $datet = purdydate();
-  $cl = IO::Socket::SSL->start_SSL(
-                                   $cl,
-                                   SSL_server          => 1,
-                                   SSL_cert_file       => $certfile,
-                                   SSL_key_file        => $keyfile,
-                                   SSL_verifycn_name   => $host,
-                                   SSL_verifycn_scheme => 'default',
-                                   SSL_hostname        => $host
-  ) or die "$datet 0x06 Error: $@";
+  $cl    = IO::Socket::SSL->start_SSL(
+    $cl,
+    SSL_server          => 1,
+    SSL_cert_file       => $certfile,
+    SSL_key_file        => $keyfile,
+    SSL_verifycn_name   => $host,
+    SSL_verifycn_scheme => 'default',
+    SSL_hostname        => $host)
+    or print LOG "$datet 0x01 Error: $@";
   # unblock
-  my $flags = fcntl($cl, F_GETFL, 0) or die purdydate() . " $cl->peerhost $!";
-  #  fcntl($cl, F_SETFL, $flags | O_NONBLOCK) or die "$datet $cl->peerhost $!";  # nonblocking code ended with half docs
-  fcntl($cl, F_SETFL, $flags) or die "$datet $cl->peerhost $!";
+  my $flags = fcntl($cl, F_GETFL, 0) or print LOG purdydate() . " 0x09 $cl->peerhost $!";
+  #  fcntl($cl, F_SETFL, $flags) or print LOG "$datet 0x0C $cl->peerhost $!";
   my $rndid    = genuniq();
   my $filename = $pasteroot . $rndid;
   $datet = purdydate();
-  print LOG $datet . " " . $cl->peerhost . "/" . $cl->peerport;
+  print LOG $datet . " 0x00 " . $cl->peerhost . "/" . $cl->peerport;
   print LOG " $rndid : storing at $pasteroot$rndid\n";
-  print "$rndid : storing at $pasteroot$rndid\n";
+  print STDOUT "$rndid : storing at $pasteroot$rndid\n";
   $datet = purdydate();
-  print LOG $datet . " " . $cl->peerhost . "/" . $cl->peerport;
+  print LOG $datet . " 0x00 " . $cl->peerhost . "/" . $cl->peerport;
   print LOG " $rndid : serving at $srvname/p/$rndid\n";
-  print "$rndid : serving at $srvname/p/$rndid\n";
+  print STDOUT "$rndid : serving at $srvname/p/$rndid\n";
+  print $cl "$srvname/p/$rndid\n";
   open(P, '>', $filename);
   my $switch = 0;
-  while (my $line = $cl->getline()) {             # i can make getline work like this
-    if ($line =~ m/[\x00\x01\x0E-\x16\x7F-\xFF]/) {   # non printable chars
+  while (my $line = $cl->getline()) {    # i can make getline work like this
+    if ($line =~ m/[\x00\x01\x0E-\x16\x7F-\xFF]/) {    # non printable chars
       print $cl "0x03 Error: Nonprintable chars not supported.";
-      print LOG purdydate() . " " . "0x03 Error: Nonprintable chars not supported.\n";
+      print LOG purdydate() . " "
+        . "0x03 Error: Nonprintable chars not supported.\n";
       unlink($filename);
       $cl->close();
       return 1;
-    }
-    else {
+    } else {
       print P $line;
     }
-if ($switch == 0) {
-  $switch = 1;
-  print $cl "$srvname/p/$rndid\n";
-  print $cl "<<END>>\n\n";
-  # $cl->close();
-}
-}
-#$cl->close();
-#}
-  #  }
-  
-
-close(P);
-  $cl->close();  # needs to be closed out way out here to avoid cutting document short
+    if ($switch == 0) {
+      $switch = 1;
+      print $cl "<<END>>\n\n";
+    }
+  }
+  close(P);
+  # needs to be closed out way out here to avoid cutting document short
+  $cl->close();   
   return 0;
 }
-
 
 sub genuniq {
   my $pasid;        # for unique paste identifier
@@ -158,27 +152,27 @@ sub genuniq {
   return $pasid;    # push it back
 }
 
-
 sub purdydate {
-
-  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
-  my $datetime = sprintf("%04d%02d%02d %02d:%02d:%02d",
-                         $year + 1900,
-                         $mon + 1, $mday, $hour, $min, $sec);
+  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) =
+    localtime(time);
+  my $datetime = sprintf(
+    "%04d%02d%02d %02d:%02d:%02d",
+    $year + 1900,
+    $mon + 1, $mday, $hour, $min, $sec);
   return $datetime;
 }
-
 
 END {
   if ($cfgf) {
     if (-e $pidfile) {
       unless ($SIG{TERM} || $SIG{INT}) {
-        print STDERR "$datet 0x02 Error: Something unusual happened... check $logfile\n";
+        print STDERR
+          "$datet 0x02 Error: Something unusual happened... check $logfile\n";
       }
-      print LOG "Removing lockfile...\n";
+      print LOG purdydate() . " 0x0B Removing lockfile...\n";
       unlink($pidfile);
     }
-    print LOG "Stopping SPaste process cleanly...\n";
+    print LOG purdydate() . " 0x00 Stopping SPaste process cleanly...\n";
   }
 }
 
